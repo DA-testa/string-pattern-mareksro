7f12630db02752e51d1e74e2fde5ea05d283e3f3
Mareks Rozenblats <mareks.rozenblats@edu.rtu.lv>
2023-04-05 02:01:31 +0300

    new

--- hash_substring.py
+++ hash_substring.py
@@ -5,18 +5,15 @@ Q=256
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    input = input()
+    tips = input()
     
     if "I" in input:
         pattern = input().rstrip()
         text = input().rstrip()
     
     if "F" in input:
-        fails = input()
-        if "a" in fails:
-            print("wrong file name")
-            return
-    with open("./test/06", "r") as f:
+        fails = "./test/06"
+    with open(fails, "r") as f:
         pattern = f.readline().rstrip()
         text = f.readline().rstrip()
     # after input type choice
